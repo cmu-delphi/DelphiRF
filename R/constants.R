3edbb0719549fee34a1a1c5b9d6eece026dc95f3
Jingjing Tang <jingjingtang@Jingjings-Mac-mini.local>
2025-05-05 00:27:55 -0400

    fix error after adding new constants

--- R/constants.R
+++ R/constants.R
@@ -73,6 +73,9 @@ TODAY <- Sys.Date()
 #' @rdname constants
 #' @export
 TEST_LAG_GROUPS_DAILY <- c(as.character(0:14), c("15-21", "22-35", "36-49", "50-59"))
+
+#' @rdname constants
+#' @export
 TEST_LAG_GROUPS_DAILY <- c(as.character(seq(0, 70, 7)))
 
 
